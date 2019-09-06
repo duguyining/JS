37d3a1858fe07f39a703a9b48ace0efef635e5a9
ZhuChao <2580500887@qq.com>
2019-09-06 16:15:55 +0800

    继承

--- JS面向对象编程/原型继承/prototype.js
+++ JS面向对象编程/原型继承/prototype.js
@@ -69,3 +69,12 @@ inherits(PrimaryStudent, Student);
 PrimaryStudent.prototype.getGrade = function () {
     return this.grade;
 };
+
+/*
+JavaScript的原型继承实现方式就是：
+
+定义新的构造函数，并在内部用call()调用希望“继承”的构造函数，并绑定this；
+
+借助中间函数F实现原型链继承，最好通过封装的inherits函数完成；
+
+继续在新的构造函数的原型上定义新方法。*/
