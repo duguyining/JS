344a0d68a135a39b5ce83c7f8fa253bbd1463dd4
ZhuChao <2580500887@qq.com>
2019-09-06 16:51:30 +0800

    浏览器

---
+++ 浏览器对象/example01.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// 可以调整浏览器窗口大小试试:
+console.log('window inner size: ' + window.innerWidth + ' x ' + window.innerHeight);
+
+console.log('appName = ' + navigator.appName);
+console.log('appVersion = ' + navigator.appVersion);
+console.log('language = ' + navigator.language);
+console.log('platform = ' + navigator.platform);
+console.log('userAgent = ' + navigator.userAgent);
