2748c79808fe8d2bff379bb6f7b52875c42e0880
ZhuChao <2580500887@qq.com>
2019-10-09 18:09:08 +0800

    函数的扩展

---
+++ ES6/函数的扩展/03-tailCall.js
@@ -0,0 +1,16 @@
+/*
+function factorial(n) {
+if (n === 1) return 1;
+return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // 120*/
+
+
+//尾递归优化
+function factorial(n,total) {
+    if (n===1) return total;
+    return factorial(n-1,n*total);
+}
+
+console.log(factorial(5, 1));;
