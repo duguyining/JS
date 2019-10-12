019e29f27c0744b006b1888f1895890e07819d19
ZhuChao <2580500887@qq.com>
2019-10-12 14:04:54 +0800

    stream

---
+++ NodeJS/NodeJs Stream/decompress.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+var zlib = require('zlib');
+
+//解压缩input.txt.gz文件为input.txt
+fs.createReadStream('input.txt.gz')
+.pipe(zlib.createGunzip()).pipe(fs.createWriteStream('input.txt'));
+
+console.log('文件解压完成');
